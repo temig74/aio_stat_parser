b4ed7f5d3c14c43de111259a1f89a434941aa590
temig74 <temig87@yandex.ru>
2024-12-06 04:02:48 +0500

    help fix

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher()
 example = '''<code>/stat https://dozorekb.en.cx/GameStat.aspx?gid=76109</code>
 <code>/stat https://dozorekb.en.cx/GameStat.aspx?gid=76109 8 15 19 25 86 89-95 99</code>
-<code>/stat https://dozorekb.en.cx/GameStat.aspx?gid=76109 1-103 -22 -35 -52 -68 -78 -79 -80</code> (уровни 1-103, за исключением 22 3552 68 78 79 80)
+<code>/stat https://dozorekb.en.cx/GameStat.aspx?gid=76109 1-103 -22 -35 -52 -68 -78 -79 -80</code> (уровни 1-103, за исключением 22 35 52 68 78 79 80)
 <code>/textstat https://dozorekb.en.cx/GameStat.aspx?gid=76109 доезд QRV</code> (только доезды и QRV)
 <code>/textstat https://dozorekb.en.cx/GameStat.aspx?gid=76109 -доезд -QRV</code> (исключить доезды и QRV)
 <code>/csv https://dozorekb.en.cx/GameStat.aspx?gid=76109</code>'''
